90b0341d2d485f0dee729bc1410ee4ac5e8e6788
Ваше имя <emmerih.maxim@gmail.com>
2019-10-24 23:01:48 +0700

    figures done

--- figures.py
+++ figures.py
@@ -1,27 +1,3 @@
-class Triangle(object):
-    def __init__(self,x,y,side,color,angle):
-        self.color = color
-        self.x = x
-        self.y = y
-        self.side = side
-        self.angle = angle
-
-    def draw(self):
-        turtle.up()
-        turtle.color(self.color)
-        turtle.right(self.angle)
-        turtle.goto(self.x, self.y)
-        turtle.down()
-        turtle.fillcolor(self.color)
-        turtle.begin_fill()
-        turtle.forward(self.side)
-        turtle.left(120)
-        turtle.forward(self.side)
-        turtle.left(120)
-        turtle.forward(self.side)
-        turtle.left(120)
-        turtle.end_fill()
-
 def sheet():
     tri1 = Triangle(90, 90, 100, 'green', 0)
     tri1.draw()
@@ -49,5 +25,27 @@ def piss():
     tri3 = Triangle(50, -1 * math.sqrt(7500), 100, 'purple', 90)
     tri3.draw()
     tri3 = Triangle(50, math.sqrt(7500), 100, 'navy', 180)
+    tri3.draw()
+
+
+def fisher():
+    tri1 = Triangle(0, 0, 100, 'gray', 0)
+    tri1.draw()
+    tri2 = Triangle(-100, 0, 100, 'gray', 0)
+    tri2.draw()
+    tri4 = Triangle(-50, math.sqrt(7500), 100, 'gray', 0)
+    tri4.draw()
+    tri3 = Triangle(-50, math.sqrt(7500), 100, 'black', 60)
+    tri3.draw()
+
+
+def lichinus():
+    tri1 = Triangle(0, 0, 100, 'cyan', 210)
+    tri1.draw()
+    tri2 = Triangle(0, 0, 100, 'teal', 180)
+    tri2.draw()
+    tri3 = Triangle(-1 * math.sqrt(7500), 50, 2 * math.sqrt(7500), 'navy', -30)
+    tri3.draw()
+    tri3 = Triangle(-1 * math.sqrt(7500), -50, 2 * math.sqrt(7500), 'yellow', 60)
     tri3.draw()
 
